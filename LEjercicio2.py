4634ef14f314e7a63f915e485068fdc1ad524acf
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-11-28 19:23:59 -0500

    Add files via upload

---
+++ LEjercicio2.py
@@ -0,0 +1,28 @@
+print("---------------------------------------------------------------------------------------------------------------")
+print("---------------------------------------------------------------------------------------------------------------")
+print("---------------------------------------------------- Ejercicio 3 ----------------------------------------------")
+print("---------------------------------------------------------------------------------------------------------------")
+print("---------------------------------------------------------------------------------------------------------------")
+#Ejercicio3
+# Los arrays `u` y `v` representan dos series en funcion del tiempo `t`.
+# Calcule la covarianza entre `u` y `v` e imprima su valor.
+
+import numpy as np
+import matplotlib.pylab as plt
+u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
+v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+
+def covarianza(u,v):
+	A=np.zeros((len(u),len(u)))
+	B=np.zeros((len(u),len(u)))
+	ab=np.array([])
+	for i in range(1,len(u)):
+		for j in range(1,len(u)):	
+			a=u[i]-np.mean(u)
+			b=v[j]-np.mean(v)	
+			ab=a*b
+			suma[i]=(np.sum(ab))/(len(u)-1)
+	return suma
+
+print covarianza(u,v)
