c9e1a883f31575851f6d7bb6be30fde66ff67bac
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-11-28 19:55:36 -0500

    Add files via upload

--- LEjercicio4.py
+++ LEjercicio4.py
@@ -9,9 +9,9 @@ print("-------------------------------------------------------------------------
 #1) Usando los paquetes de scipy de la transformada de Fourier, haga un FILTRO de la senial que elimine las frecuencias mayores a 1000Hz en las senial original.
 #2) Haga una grafica de la senial original y la senial filtarada y guardela SIN MOSTRARLA en filtro.pdf
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pylab as plt
 from scipy.fftpack import fft, fftfreq, ifft
 
 n = 1280 # number of point in the whole interval
@@ -21,25 +21,22 @@ t = np.linspace( 0, (n-1)*dt, n)
 amp = np.cos(2 * np.pi * f * t) - 0.4 * np.sin(2 * np.pi * (2*f) * t ) + np.random.random(n)
 
 # SU FILTRO
-fft_x=fft(amp)
-freq=fftfreq(n)
+fft_x=fft(amp)/n
+freq=fftfreq(n,dt)
+print np.shape(fft_x)
 
 def filtro(fft_x):
 	for i in range((np.shape(fft_x))[0]):
-		if(freq[i]>1000):
+		if(np.abs(freq[i])>1000):	
 			fft_x[i]=0
-		if(freq[i]<1000):
-			fft_x[i]=0
-
 	return fft_x
 
 inversa= ifft(filtro(fft_x))
-print fft_x, freq
+print fft_x, freq, inversa
 # SU GRAFICA
 
 plt.figure()
-plt.plot(freq,amp)
-plt.plot(freq,fft_x)
+plt.plot(freq,inversa)  
 plt.savefig("filtro.pdf")
 
 # Puede usar los siguientes paquetes:
